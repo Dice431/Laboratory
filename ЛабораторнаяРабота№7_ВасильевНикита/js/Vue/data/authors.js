cd8b0e895c0936e6499d951327aeb27df9b23139
Васильев Никита <93082595+Dice431@users.noreply.github.com>
2022-11-16 19:13:01 +0300

    Add files via upload

---
+++ ЛабораторнаяРабота№7_ВасильевНикита/js/Vue/data/authors.js
@@ -0,0 +1,31 @@
+let list_authors = [
+    {
+        'name': 'Юзьеров Константин Агапович',
+        'login': '@OtherUserAccountName',
+        'subscribers': '5',
+        'signed': '4',
+        'post_count': '108'
+    },
+    {
+        'name': 'Васильев Никита Васильевич',
+        'login': '@NikitaVasilyev',
+        'subscribers': '13',
+        'signed': '13',
+        'post_count': '50'
+    }
+
+];
+
+const CreateOptions = () => {
+    let select = document.querySelector('#select_authors');
+
+    list_authors.forEach(it => {
+        let opt = document.createElement('option');
+        opt.value = it.login;
+        opt.innerHTML = it.name;
+
+        select.appendChild(opt);
+    });
+};
+
+CreateOptions();
